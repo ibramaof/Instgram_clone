f7616d941538f0106afd90d66d5199482053929e
Ibrahim Abdelrahman <ibrahimaof3@gmail.com>
2025-12-19 20:24:12 +0200

    modified version using js

--- index.js
+++ index.js
@@ -28,3 +28,46 @@ const posts = [
     }
 ]
 
+let name = document.getElementById("name")
+let userName = document.getElementById("user-name")
+let loCation = document.getElementById("location")
+let avatar = document.getElementById("post-user-img")
+let post = document.getElementById("post-img")
+let comment = document.getElementById("caption")
+let likesCount = document.getElementById("like-count")
+
+
+
+name.innerHTML = posts[1]["name"]
+userName.innerHTML = posts[1]["username"]
+loCation.innerHTML = posts[1]["location"]
+avatar.src = posts[1]["avatar"]
+post.src = posts[1]["post"]
+comment.innerHTML = posts[1]["comment"]
+likesCount.innerHTML = `${posts[1]["likes"]} likes`
+
+let liked = document.getElementById("icons-heart")
+let isLiked = false
+
+post.addEventListener("dblclick", function () {
+    if (!isLiked) {
+        liked.src = "images/heart.png"
+        isLiked = true
+    } else {
+        liked.src = "images/icon-heart.png"
+        isLiked = false
+    }
+})
+
+liked.addEventListener("click", function () {
+    if (!isLiked) {
+        liked.src = "images/heart.png"
+        isLiked = true
+    } else {
+        liked.src = "images/icon-heart.png"
+        isLiked = false
+    }
+
+})
+
+
