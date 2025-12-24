5d93265b0b3112b091caa02de3dfed9205552a19
Ibrahim Abdelrahman <ibrahimaof3@gmail.com>
2025-12-24 18:18:47 +0200

    refactor the app using DOM instead of hard coding still the likes logic is not working

--- index.js
+++ index.js
@@ -1,98 +1,60 @@
-const posts = [
-    {
-        name: "Vincent van Gogh",
-        username: "vincey1853",
-        location: "Zundert, Netherlands",
-        avatar: "images/avatar-vangogh.jpg",
-        post: "images/post-vangogh.jpg",
-        comment: "just took a few mushrooms lol",
-        likes: 21
-    },
-    {
-        name: "Gustave Courbet",
-        username: "gus1819",
-        location: "Ornans, France",
-        avatar: "images/avatar-courbet.jpg",
-        post: "images/post-courbet.jpg",
-        comment: "i'm feelin a bit stressed tbh",
-        likes: 4
-    },
-    {
-        name: "Joseph Ducreux",
-        username: "jd1735",
-        location: "Paris, France",
-        avatar: "images/avatar-ducreux.jpg",
-        post: "images/post-ducreux.jpg",
-        comment: "gm friends! which coin are YOU stacking up today?? post below and WAGMI!",
-        likes: 152
+import { posts } from '/data.js'
+
+
+//variables
+const main = document.getElementById("posts-el")
+
+
+
+
+//post
+
+function renderPost() {
+    let renderPost = ''
+    for (let postEl in posts) {
+        renderPost += `
+        <section class="post">
+            <div class="post-header">
+                <div>
+                    <img id="post-user-img" class="post-user-img" src="${posts[postEl].avatar}" alt="avatar-courbet">
+                </div>
+                <div class="post-header-typography">
+                    <p id="name" class="name">${posts[postEl].name}</p>
+                    <p id="location" class="location">${posts[postEl].location}</p>
+                </div>
+            </div>
+            <div class="post-container-img">
+                <img id="post-img" class="post-img" src="${posts[postEl].post}" alt="post - courbet">
+            </div>
+            <div class="post-footer">
+                <div class="icons">
+                    <img id="icons-heart" class="liked" src="images/icon-heart.png" alt="icon-heart">
+                        <img id="icons-comment" src="images/icon-comment.png" alt="icon-comment">
+                            <img id="icons-dm" src="images/icon-dm.png" alt="icon-dm">
+                            </div>
+                            <div class="post-info">
+                                <p id="like-count" class="likes">${posts[postEl].likes} Likes</p>
+                                <p class="post-caption">
+                                    <span id="user-name" class="username">${posts[postEl].username}</span>
+                                    <span id="caption" class="caption">${posts[postEl].comment}</span>
+                                </p>
+                            </div>
+                        </div>
+                    </section>
+                    `
     }
-]
+    main.innerHTML = renderPost
+}
 
-const name = document.getElementById("name")
-const userName = document.getElementById("user-name")
-const loCation = document.getElementById("location")
-const avatar = document.getElementById("post-user-img")
-const post = document.getElementById("post-img")
-const comment = document.getElementById("caption")
+renderPost()
+
+// like logic
+const imgPost = document.getElementById("post-img")
 const likesCount = document.getElementById("like-count")
-
-
-const name1 = document.getElementById("name1")
-const userName1 = document.getElementById("user-name1")
-const loCation1 = document.getElementById("location1")
-const avatar1 = document.getElementById("post-user-img1")
-const post1 = document.getElementById("post-img1")
-const comment1 = document.getElementById("caption1")
-const likesCount1 = document.getElementById("like-count1")
-
-const name2 = document.getElementById("name2")
-const userName2 = document.getElementById("user-name2")
-const loCation2 = document.getElementById("location2")
-const avatar2 = document.getElementById("post-user-img2")
-const post2 = document.getElementById("post-img2")
-const comment2 = document.getElementById("caption2")
-const likesCount2 = document.getElementById("like-count2")
-
-
-name.innerHTML = posts[0]["name"]
-userName.innerHTML = posts[0]["username"]
-loCation.innerHTML = posts[0]["location"]
-avatar.src = posts[0]["avatar"]
-post.src = posts[0]["post"]
-comment.innerHTML = posts[0]["comment"]
-likesCount.innerHTML = `${posts[0]["likes"]} likes`
-
-name1.innerHTML = posts[1]["name"]
-userName1.innerHTML = posts[1]["username"]
-loCation1.innerHTML = posts[1]["location"]
-avatar1.src = posts[1]["avatar"]
-post1.src = posts[1]["post"]
-comment1.innerHTML = posts[1]["comment"]
-likesCount1.innerHTML = `${posts[1]["likes"]} likes`
-
-name2.innerHTML = posts[2]["name"]
-userName2.innerHTML = posts[2]["username"]
-loCation2.innerHTML = posts[2]["location"]
-avatar2.src = posts[2]["avatar"]
-post2.src = posts[2]["post"]
-comment2.innerHTML = posts[2]["comment"]
-likesCount2.innerHTML = `${posts[2]["likes"]} likes`
-
-
-
-
-
-
-
-
-
-
-// icons logic
-
 const liked = document.getElementById("icons-heart")
 let isLiked = false
 
-post.addEventListener("dblclick", function () {
+imgPost.addEventListener("dblclick", function () {
     if (!isLiked) {
         liked.src = "images/heart.png"
         isLiked = true
@@ -121,69 +83,6 @@ liked.addEventListener("click", function () {
 
 })
 
-const liked1 = document.getElementById("icons-heart1")
-let isLiked1 = false
 
-post1.addEventListener("dblclick", function () {
-    if (!isLiked1) {
-        liked1.src = "images/heart.png"
-        isLiked1 = true
-        posts[1].likes += 1
-        likesCount1.innerHTML = `${posts[1]["likes"]} likes`
-    } else {
-        liked1.src = "images/icon-heart.png"
-        isLiked1 = false
-        posts[1].likes -= 1
-        likesCount1.innerHTML = `${posts[1]["likes"]} likes`
-    }
-})
-
-liked1.addEventListener("click", function () {
-    if (!isLiked1) {
-        liked1.src = "images/heart.png"
-        isLiked1 = true
-        posts[1].likes += 1
-        likesCount1.innerHTML = `${posts[1]["likes"]} likes`
-    } else {
-        liked1.src = "images/icon-heart.png"
-        isLiked1 = false
-        posts[1].likes -= 1
-        likesCount1.innerHTML = `${posts[1]["likes"]} likes`
-    }
-
-})
-
-
-const liked2 = document.getElementById("icons-heart2")
-let isLiked2 = false
-
-post2.addEventListener("dblclick", function () {
-    if (!isLiked2) {
-        liked2.src = "images/heart.png"
-        isLiked2 = true
-        posts[2].likes += 1
-        likesCount2.innerHTML = `${posts[2]["likes"]} likes`
-    } else {
-        liked2.src = "images/icon-heart.png"
-        isLiked2 = false
-        posts[2].likes -= 1
-        likesCount2.innerHTML = `${posts[2]["likes"]} likes`
-    }
-})
-
-liked2.addEventListener("click", function () {
-    if (!isLiked2) {
-        liked2.src = "images/heart.png"
-        isLiked2 = true
-        posts[2].likes += 1
-        likesCount2.innerHTML = `${posts[2]["likes"]} likes`
-    } else {
-        liked2.src = "images/icon-heart.png"
-        isLiked2 = false
-        posts[2].likes -= 1
-        likesCount2.innerHTML = `${posts[2]["likes"]} likes`
-    }
-
-})
 
 
